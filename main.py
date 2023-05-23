0c229d9fa84074dd52c24f6135ca99751bc4cc76
Sharapatov Nikita <neffshar@gmail.com>
2023-05-24 00:58:56 +0300

    def_remove_comment

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-import solcx
-
+import re
+import solcast
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
+def remove_comment(text):
+    pattern = r"/\*\*.*?\*/"
+    clean_text = re.sub(pattern, '', text, flags=re.DOTALL)
+
+    pattern = r"/\*.*?\*/"
+    clean_text = re.sub(pattern, '', clean_text, flags=re.DOTALL)
+
+    pattern = r"//.*?$"
+    clean_text = re.sub(pattern, '', clean_text, flags=re.MULTILINE)
+
+    return clean_text
 def get_code (url):
     response = requests.get((url), headers=headers)
     if response.ok:
@@ -11,7 +22,6 @@ def get_code (url):
         pre_tags = soup.find('pre')
         for pre_tag in pre_tags:
             contract_code = pre_tag.get_text()
-        return contract_code
+        return print(remove_comment(contract_code))
     else:
         raise ValueError(f"Failed to fetch contract source code from {url}")
-get_code('https://etherscan.io/address/0x4B274807e2Cf091eAFCe26390f7FBeC626D4b0ab')
